2cec38ac773e6b06eed9d3d1d85fe6a8c3d3b89c
Kamil Piwko <iz.podpolja@gmail.com>
2023-02-01 22:58:13 +0100

    downgrade rails to 3.1.2; add a rails test page view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :doodads
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/page", to: "page#index" 
   # Defines the root path route ("/")
   # root "articles#index"
 end
