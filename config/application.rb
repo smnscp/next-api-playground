ae437496df973f08a5f9a730fcbbbc5d0ae83e6f
Kamil Piwko <iz.podpolja@gmail.com>
2023-02-01 17:02:51 +0100

    disable host protection

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module NextApiPlayground
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.hosts.clear
   end
 end
