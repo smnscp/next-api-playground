8bae305f8e000cef8778dacecadbeb5cccc978e9
Kamil Piwko <iz.podpolja@gmail.com>
2023-02-02 09:56:16 +0100

    add rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  rewrites: async () => ({
+    fallback: [
+      {
+        source: "/packs/:path*",
+        destination: `${process.env.HOST_API}/packs/:path*`,
+      },
+      {
+        source: "/:path*",
+        destination: `${process.env.HOST_API}/:path*`,
+      },
+    ],
+  }),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
